6e86e1c831fd5cb236fc0fcf31b14de6eb49c4ce
JoInJae <andinjai@naver.com>
2022-02-23 10:29:15 +0900

    commit Auth

--- src/main/java/rowan/app/mvc/web/IndexController.java
+++ src/main/java/rowan/app/mvc/web/IndexController.java
@@ -1,6 +1,7 @@
 package rowan.app.mvc.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -20,7 +21,9 @@ public class IndexController {
 
     }
 
+
     @RequestMapping(value = "/route/**", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.TEMPORARY_REDIRECT)
     private ModelAndView route(HttpServletRequest request, @RequestAttribute(name = "service") Services service, RedirectAttributes redirectAttributes){
 
         String path = "https://api.super-brain.co.kr" + request.getServletPath().replaceFirst("route", service.getPath());
